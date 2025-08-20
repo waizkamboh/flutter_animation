fc88dbadd9895bb60fdd7a4039d94e7a8ce14011
waizkamboh <waizkamboh6@gmail.com>
2025-08-20 08:33:39 +0500

    add images to show liquid swipe animation

--- lib/liquid_swipe_animation.dart
+++ lib/liquid_swipe_animation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class LiquidSwipeAnimation extends StatefulWidget {
   const LiquidSwipeAnimation({super.key});
@@ -13,7 +14,68 @@ class _LiquidSwipeAnimationState extends State<LiquidSwipeAnimation> {
 
   final controller = LiquidController();
 
-
+  final pages = [
+    SizedBox(
+      height: double.infinity,
+      child: CachedNetworkImage(
+        imageUrl:
+        'https://plus.unsplash.com/premium_photo-1675200124904-dfadce24119f?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        fit: BoxFit.cover,
+        placeholder: (context, url) =>
+        const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) =>
+        const Center(child: Icon(Icons.error)),
+      ),
+    ),
+    SizedBox(
+      height: double.infinity,
+      child: CachedNetworkImage(
+        imageUrl:
+        'https://plus.unsplash.com/premium_photo-1668485968521-4e182e8093d8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        fit: BoxFit.cover,
+        placeholder: (context, url) =>
+        const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) =>
+        const Center(child: Icon(Icons.error)),
+      ),
+    ),
+    SizedBox(
+      height: double.infinity,
+      child: CachedNetworkImage(
+        imageUrl:
+        'https://images.unsplash.com/photo-1578979879663-4ba6a968a50a?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        fit: BoxFit.cover,
+        placeholder: (context, url) =>
+        const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) =>
+        const Center(child: Icon(Icons.error)),
+      ),
+    ),
+    SizedBox(
+      height: double.infinity,
+      child: CachedNetworkImage(
+        imageUrl:
+        'https://images.unsplash.com/photo-1521567097888-2c5fc40a8660?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTl8R0UzbVRqanpQRWd8fGVufDB8fHx8fA%3D%3D',
+        fit: BoxFit.cover,
+        placeholder: (context, url) =>
+        const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) =>
+        const Center(child: Icon(Icons.error)),
+      ),
+    ),
+    SizedBox(
+      height: double.infinity,
+      child: CachedNetworkImage(
+        imageUrl:
+        'https://images.unsplash.com/photo-1524626050212-92f72d69a67b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        fit: BoxFit.cover,
+        placeholder: (context, url) =>
+        const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, url, error) =>
+        const Center(child: Icon(Icons.error)),
+      ),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,20 +90,7 @@ class _LiquidSwipeAnimationState extends State<LiquidSwipeAnimation> {
              });
             },
             slideIconWidget: Icon(Icons.arrow_back_ios,color: Colors.white,),
-              pages: [
-                 Container(
-                   color: Colors.black,
-                   child: Center(child: Text('Pgae1'),),
-                 ),
-                 Container(
-                   color: Colors.blue.shade800,
-                   child: Center(child: Text('Pgae2'),),
-                 ),
-                 Container(
-                   color: Colors.deepPurple.shade900,
-                   child: Center(child: Text('Pgae3'),),
-                 ),
-              ]
+              pages: pages
           ),
           Positioned(
             bottom: 0,
