4e345111e70666b747255f52e3a918bf2fede4bc
waizkamboh <waizkamboh6@gmail.com>
2025-08-21 12:10:24 +0500

    add assets

---
+++ lib/List Scroll/Model/scrol_list_model.dart
@@ -0,0 +1,77 @@
+class ScrollItems {
+  final String image;
+  final String name;
+  final int color;
+
+  ScrollItems({
+    required this.image,
+    required this.name,
+    required this.color,
+  });
+}
+
+final dataItems= <ScrollItems>[
+   ScrollItems(
+    name: "Chicken",
+    image: "assets/images/chicken.png",
+    color: 0xFFFFA4D8,
+  ),
+  ScrollItems(
+    name: "Burger",
+    image: "assets/images/burger.png",
+    color: 0xFF99C5FD,
+  ),
+  ScrollItems(
+    name: "Noodles",
+    image: "assets/images/noodles.png",
+    color: 0xFF40AC9C,
+  ),
+  ScrollItems(
+    name: "Lemon",
+    image: "assets/images/lemon.png",
+    color: 0xFFD6F670,
+  ),
+  ScrollItems(
+    name: "Rum",
+    image: "assets/images/rum.png",
+    color: 0xFFE7668E,
+  ),
+  
+  ScrollItems(
+    name: "Cheese",
+    image: "assets/images/cheese.png",
+    color: 0xFF99C5FD,
+  ),
+
+  ScrollItems(
+    name: "CocaCola",
+    image: "assets/images/coca cola.png",
+    color: 0xFFFFA4D8,
+  ),
+  ScrollItems(
+    name: "assets/Ice Cream",
+    image: "images/ice_cream.png",
+    color: 0xFF447C12,
+  ),
+ 
+  ScrollItems(
+    name: "Pizza",
+    image: "assets/images/pizza.png",
+    color: 0xFFB1D1FF,
+  ),
+  ScrollItems(
+    name: "Chicken",
+    image: "assets/images/chicken.png",
+    color: 0xFFFFA4D8,
+  ),
+  ScrollItems(
+    name: "Burger",
+    image: "assets/images/burger.png",
+    color: 0xFF99C5FD,
+  ),
+  ScrollItems(
+    name: "Noodles",
+    image: "assets/images/noodles.png",
+    color: 0xFF40AC9C,
+  ),
+];
